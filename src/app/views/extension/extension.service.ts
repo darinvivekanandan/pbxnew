99483ba472d45d377b7ae80b3617f3e7519a1a60
rahulmukundan <rahulmukundan999@gmail.com>
2018-10-13 16:40:23 +0530

    commited

--- src/app/views/extension/extension.service.ts
+++ src/app/views/extension/extension.service.ts
@@ -20,20 +20,20 @@ getExtensions(userid)
   var user = {
     userid: userid
   }
-  return this.http.get('http://localhost:3000/api/extensions',{params:user}).pipe(
+  return this.http.get('http://104.248.51.139:3000/api/extensions',{params:user}).pipe(
   map(res => res.json()));
 }
 addExtension(newExtension)
 {
   var headers = new Headers();
   headers.append('Content-Type','Application/Json');
-  return this.http.post('http://localhost:3000/api/extension',newExtension,{headers:headers}).pipe(
+  return this.http.post('http://104.248.51.139:3000/api/extension',newExtension,{headers:headers}).pipe(
   map(res => res.json()));
 }
 
 deleteExtension(id,extension)
 {
-  return this.http.delete('http://localhost:3000/api/extension/'+id+'/'+extension).pipe(map(res => res.json()));
+  return this.http.delete('http://104.248.51.139:3000/api/extension/'+id+'/'+extension).pipe(map(res => res.json()));
 }
 
 
